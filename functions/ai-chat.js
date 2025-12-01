d0fd96155fb0e93b04d6cf4c9b2dc0fffc8d9370
mdyahhya <142885726+mdyahhya@users.noreply.github.com>
2025-12-01 16:09:20 +0530

    Update ai-chat.js

--- functions/ai-chat.js
+++ functions/ai-chat.js
@@ -23,7 +23,7 @@ exports.handler = async (event, context) => {
   const HF_TOKEN = process.env.HF_TOKEN;
   
   // Choose one of these models:
-  const MODEL_API = 'https://router.huggingface.co/models/google/gemma-2-2b-it';
+  const MODEL_API = 'https://router.huggingface.co/models/google/gemma-2-2b';
   // https://router.huggingface.co/models/meta-llama/Llama-3.2-3B-Instruct';
   // OR: 'https://router.huggingface.co/models/microsoft/Phi-3-mini-4k-instruct'
   // OR: 'https://router.huggingface.co/models/google/gemma-2-2b-it'
